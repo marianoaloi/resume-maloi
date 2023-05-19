c522786ad93dba1e62ada4a28571d1bdb3aed162
Mariano Aloi <mariano@aloi.com.br>
2023-05-19 17:48:00 -0300

    better windows compile

--- deploy.ps1
+++ deploy.ps1
@@ -15,7 +15,7 @@ Copy-Item package.json  ../resume-executable/
 C:\Python310\python.exe ../cleanNPMFile.py ../resume-executable/package.json
 Set-Location ../resume-executable/
 npm install --save-dev electron
-npm install electron-context-menu
+npm install --save-dev electron-context-menu
 npm install --save-dev electron-packager
 npm install --save-dev electron-winstaller
 npm run buildele
