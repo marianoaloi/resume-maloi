8257470440f2b19040061ccdd049ca4e015c32a7
Mariano Aloi <mariano@aloi.com.br>
2023-05-19 17:21:20 -0300

    better script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 ## ./bash
+sed -i 's/\(^[^\/]*Remote\)/\/\/\1/g'  ./node_modules/ngx-electron/lib/electron.service.d.ts
 npm run build
 rm -r ../resume-executable/
 mkdir ../resume-executable/
@@ -11,5 +12,8 @@ sed -i 's/.*openDevTools.*/\/\//g' ../resume-executable/app.js
 python3 ../cleanNPMFile.py ../resume-executable/package.json
 cd ../resume-executable/
 npm install --save-dev electron
+npm install --save-dev electron-context-menu
+npm install --save-dev electron-packager
+npm install --save-dev electron-installer-debian
 npm run buildele
 npm run deb64
