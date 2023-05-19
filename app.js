a704a720912cd9c1a83e0072488f878fc737a7e6
Mariano Aloi <mariano@aloi.com.br>
2023-05-19 18:16:33 -0300

    habilitar electron (#6)
    * begin the transformation
    
    * Work very well the resume for project
    
    * remove angular analises
    
    * Better interface without table
    
    * disable analises
    
    * Fix empty substring
    fix formControlName of effect
    
    * Script automation
    
    * 0.0.1
    
    * automatization create executable
    With spell check
    
    * Version stable
    
    * better script
    
    * better ununtu script
    
    * Go to Master (#5)
    
    * Create webpack.yml
    
    * Update webpack.yml
    
    * put the --force argument
    
    * include npm install -g webpack webpack-cli --save-dev
    
    * remove webpack
    
    * Removido o webpack
    
    * better ubuntu pipeline
    
    * better windows compile
    
    * Fazer release

--- app.js
+++ app.js
@@ -1,106 +1,121 @@
-const { app, BrowserWindow, ipcMain, dialog } = require('electron')
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 const url = require("url");
 const path = require("path");
-const fs = require("fs")
+const fs = require("fs");
 
-let mainWindow
+const contextMenu = require('electron-context-menu');
+
+let mainWindow;
+
+contextMenu({
+	showSaveImageAs: true
+});
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        icon: path.join(__dirname, `/dist/resume-maloi/favicon.ico`),
-        webPreferences: {
-            // nodeIntegration: true,
-            // contextIsolation: true,
-            nodeIntegration: true, enableRemoteModule: true, contextIsolation: false
-            ,
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: path.join(__dirname, `/dist/resume-maloi/favicon.ico`),
+    webPreferences: {
+      // nodeIntegration: true,
+      // contextIsolation: true,
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      contextIsolation: false,
+      spellcheck: true,
+    },
+  });
 
-            spellcheck: true
-        }
-    })
+  mainWindow.loadFile(path.join(__dirname, `/dist/resume-maloi/index.html`));
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
 
-    mainWindow.loadFile(path.join(__dirname, `/dist/resume-maloi/index.html`));
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools()
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
 
-    mainWindow.on('closed', function () {
-        mainWindow = null
-    })
+  // const session = mainWindow.webContents.session;
+  // session.getSpellCheckerLanguages(["en"]);
+  // session.setSpellCheckerLanguages(["en"]);
+  // mainWindow.webContents.on("context-menu", (event, params) => {
+  //   console.log("context-menu event : ", event);
+  //   console.log("context-menu param : ", params);
+  // });
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
 
-app.on('activate', function () {
-    if (mainWindow === null) createWindow()
-})
+app.on("activate", function () {
+  if (mainWindow === null) createWindow();
+});
 var fileGlobal;
-var lastSaved = new Date()
-ipcMain.on('open', async (event, dataScreen) => {
-
-    let options = { filters: ["*.json"], properties: ['openFile'], title: "Open your resume" }
-    if (fileGlobal)
-        options["defaultPath"] = fileGlobal
-    let files = await dialog.showOpenDialog(options)
-    if (!files.canceled) {
-        fileGlobal = files.filePaths[0]
-        fs.watch(fileGlobal, (eventType, filename) => {
-            fs.stat(fileGlobal, function (err, stats) {
-                if (stats) {
-                    var mtime = stats.mtime;
-                    if ((lastSaved) != (mtime)) {
-                        console.log(`Evento ${eventType} arquivo mudado as ${mtime.getTime()} e tem no sistema lastSaved ${lastSaved.getTime()}`);
-                        openfile(event)
-                    }
-                }
-            });
-        })
-        openfile(event)
-
-    }
-
-})
+var lastSaved = new Date();
+ipcMain.on("open", async (event, dataScreen) => {
+  let options = {
+    filters: ["*.json"],
+    properties: ["openFile"],
+    title: "Open your resume",
+  };
+  if (fileGlobal) options["defaultPath"] = fileGlobal;
+  let files = await dialog.showOpenDialog(options);
+  if (!files.canceled) {
+    fileGlobal = files.filePaths[0];
+    fs.watch(fileGlobal, (eventType, filename) => {
+      fs.stat(fileGlobal, function (err, stats) {
+        if (stats) {
+          var mtime = stats.mtime;
+          if (lastSaved != mtime) {
+            console.log(
+              `Evento ${eventType} arquivo mudado as ${mtime.getTime()} e tem no sistema lastSaved ${lastSaved.getTime()}`
+            );
+            openfile(event);
+          }
+        }
+      });
+    });
+    openfile(event);
+  }
+});
 
 const openfile = (event) => {
-    fs.readFile(fileGlobal, 'utf8', (err, data) => {
-        if (err)
-            console.error(err)
-        else {
-            event.sender.send('fileToOpen', JSON.parse(data));
-
-        }
-    })
-}
-
-ipcMain.on('save', async (event, dataScreen, autosave) => {
-    let data = dataScreen
-    if (autosave && fileGlobal) {
-        save(fileGlobal, data, event)
-    } else {
-        let options = { filters: ["*.json"], properties: ['saveFile',], title: "Save your resume" }
-        if (fileGlobal)
-            options["defaultPath"] = fileGlobal
-        let files = await dialog.showSaveDialog(options)
-        if (!files.canceled) {
-            let file = fileGlobal = files.filePath
-
-            save(file, data, event)
-
-        }
+  fs.readFile(fileGlobal, "utf8", (err, data) => {
+    if (err) console.error(err);
+    else {
+      event.sender.send("fileToOpen", JSON.parse(data));
     }
+  });
+};
 
-})
+ipcMain.on("save", async (event, dataScreen, autosave) => {
+  let data = dataScreen;
+  if (autosave && fileGlobal) {
+    save(fileGlobal, data, event);
+  } else {
+    let options = {
+      filters: ["*.json"],
+      properties: ["saveFile"],
+      title: "Save your resume",
+    };
+    if (fileGlobal) options["defaultPath"] = fileGlobal;
+    let files = await dialog.showSaveDialog(options);
+    if (!files.canceled) {
+      let file = (fileGlobal = files.filePath);
+
+      save(file, data, event);
+    }
+  }
+});
 
 const save = (file, data, event) => {
-    lastSaved = new Date()
-    fs.writeFile(file, data, (err) => {
-        if (err) throw err;
-        // console.log('Data written to file ', lastSaved);
-    })
+  lastSaved = new Date();
+  fs.writeFile(file, data, (err) => {
+    if (err) throw err;
+    // console.log('Data written to file ', lastSaved);
+  });
 
-    event.sender.send('fileTosave', `File ${file} saved`);
-}
+  event.sender.send("fileTosave", `File ${file} saved`);
+};
